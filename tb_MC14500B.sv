a53e1549382d25f1170b6a0522458891808424aa
Ivan <ivan.shevchenko@ucu.edu.ua>
2022-11-02 18:27:52 +0200

    test bench fix

--- tb_MC14500B.sv
+++ tb_MC14500B.sv
@@ -24,29 +24,23 @@ module MC14500B_tests();
 		program_code[1] = 12'hAff;
 		program_code[2] = 12'hBff;
 		program_code[3] = 12'h800;
-		program_code[4] = 12'h801;
-		program_code[5] = 12'h000;
-		program_code[6] = 12'h200;
-		program_code[7] = 12'h401;
-		program_code[8] = 12'h800;
-		program_code[9] = 12'h801;
-		program_code[10] = 12'h802;
-		program_code[11] = 12'h000;
-		program_code[12] = 12'hC05;
-		program_write = '1;
+		program_code[4] = 12'h700;
+		program_code[5] = 12'h2ff;
+		program_code[6] = 12'h800;
+		program_code[7] = 12'h801;
+		program_code[8] = 12'h802;
+		program_code[9] = 12'hC04;
+		#(20);
 		rst = '1;
-		#(50) clk = '0;
-		#(50) clk = '1;
+		#(50);
 		rst = '0;
-		for (i=0; i<13; i=i+1) begin
+		for (i=0; i<10; i=i+1) begin
 			program_cmd = program_code[i];
-			#(50) clk = '0;
-			#(50) clk = '1;
+			program_write = '1;
+			#(50) program_write = '0;
+			#(50);
 		end
 		rst = '1;
-		#(50) clk = '0;
-		#(50) clk = '1;
-		rst = '0;
-		program_write = '0;
+		#(100) rst = '0;
 	end
 endmodule
